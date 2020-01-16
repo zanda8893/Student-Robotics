bf8a86e1d77f76a007e2754edc416671d76a73a1
zanda <zanda.hallard@outlook.com>
2020-01-16 18:10:13 +0000

    Work on webcam

--- component-test/Webcam.py
+++ component-test/Webcam.py
@@ -1,9 +1,5 @@
 from sr.robot import *
-R = Robot()
-def setWheels(a,b):
-    R.motors[0].m0.power = a
-    R.motors[0].m1.power = -b
-
+'''
 while True:
     markers = R.see()
     print "I can see", len(markers), "markers:"
@@ -17,3 +13,26 @@ while True:
             setWheels(100,100)
         else:
             setWheels(0,0)
+'''
+class robit():
+    def __init__(self, arg):
+        self.R = Robot()R = Robot()
+        super(robit, self).__init__()
+        self.arg = arg
+
+    def setWheels(self,a,b):
+        self.R.motors[0].m0.power = a
+        R.motors[0].m1.power = -b
+
+    def see(self):
+        markers = R.see()
+            if len(markers) < 1:
+                setWheels(100,-100)
+
+
+'''
+camara scans stuff
+store deets
+wheels look at deets
+'''
+robit = robit()
