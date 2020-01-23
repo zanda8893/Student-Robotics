259d505e81d41a79429bfe2c8a9b9b1e1add7beb
Shadecorp1 <59394528+Shadecorp1@users.noreply.github.com>
2020-01-23 17:14:58 +0000

    Has the x,y,bearing,and color of a cube from the robot

--- robot/cube.py
+++ robot/cube.py
@@ -1,8 +1,9 @@
 import math
 class Cube():
     def __init__(self,marker,rx,ry,ra):
-            self.x =
-            self.y =
-            self.bearing =
-            self.color =
-        
+            self.x = rx + (math.sin(ra + p.polar.rot_x))
+            self.y = ry + (math.cos(ra + p.polar.rot_y))
+            self.bearing = rot_y - (90 - p.polar.rot_y)
+            self.color = marker.info.marker_type
+    def distance(self,marker):
+        marker.dist
