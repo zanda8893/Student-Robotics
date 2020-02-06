85534568b8fae7930902a08068011f999ab932cb
Shadecorp1 <59394528+Shadecorp1@users.noreply.github.com>
2020-02-06 16:51:57 +0000

    added a list of variables and vision functions.

--- robot/cube.py
+++ robot/cube.py
@@ -1,9 +1,9 @@
 import math
+
 class Cube():
     def __init__(self,marker,rx,ry,ra):
-            self.x = rx + (math.sin(ra + p.polar.rot_x))
-            self.y = ry + (math.cos(ra + p.polar.rot_y))
-            self.bearing = 180 - p.orientation.rot_y - p.polar.rot_y - ra
+            self.x = rx + (math.cos(ra + p.polar.rot_y) * p.polar.length)
+            self.y = ry + (math.sin(ra + p.polar.rot_y) * p.polar.length)
+            self.bearing = p.orientation.rot_y - (90 - p.polar.rot_y - ra)
             self.color = marker.info.marker_type
-    def distance(self,marker):
-        marker.dist
+            self.dist = p.polar.length
