c0a536bbaedeeda0b5046765d5b520b617a11212
Shadecorp1 <59394528+Shadecorp1@users.noreply.github.com>
2020-02-06 17:11:49 +0000

    added dist to the cube

--- robot/cube.py
+++ robot/cube.py
@@ -1,9 +1,12 @@
 import math
+import robot
 
 class Cube():
     def __init__(self,marker,rx,ry,ra):
-            self.x = rx + (math.cos(ra + p.polar.rot_y) * p.polar.length)
-            self.y = ry + (math.sin(ra + p.polar.rot_y) * p.polar.length)
+            self.x = rx + (math.cos(ra + (p.polar.rot_y * 3.1415)/180) * p.polar.length)
+            self.y = ry + (math.sin(ra + (p.polar.rot_y * 3.1415)/180) * p.polar.length)
             self.bearing = p.orientation.rot_y - (90 - p.polar.rot_y - ra)
             self.color = marker.info.marker_type
-            self.dist = p.polar.length
+
+    def dist(self,x,y,rx,ry):
+        dist = ((rx-x)**2 + (ry-y)**2)**0.5
