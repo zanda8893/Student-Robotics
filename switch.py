d794a99f94b4ea78368ab9e162f015602adeca45
Shadecorp1 <59394528+Shadecorp1@users.noreply.github.com>
2020-03-06 16:05:57 +0000

    made switch a function

--- switch.py
+++ switch.py
@@ -1,11 +1,12 @@
 import boolean
 import robot
 
-R.ruggeduinos[0].pin_mode(2, INPUT_PULLUP)
+def switch(robot):
+    R.ruggeduinos[0].pin_mode(2, INPUT_PULLUP)
 
-while True:
-    if R.ruggeduinos[0].digital_read(2) == False:
-        clamped = True
+    while True:
+        if R.ruggeduinos[0].digital_read(2) == False:
+            clamped = True
 
-    else:
-        clamped = False
+        else:
+            clamped = False
