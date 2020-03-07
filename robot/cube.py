682609d16269521f78be25492bf8754b400d4f6c
Shadecorp1 <59394528+Shadecorp1@users.noreply.github.com>
2020-03-07 17:28:43 +0000

    bgbkulnk

--- robot/cube.py
+++ robot/cube.py
@@ -1,13 +1,9 @@
 import math
-from sr.robot import *
-
-
 class Cube():
     def __init__(self,marker,rx,ry,ra):
-            self.x = rx + (math.cos(ra + (p.polar.rot_y * 3.1415)/180) * p.polar.length)
-            self.y = ry + (math.sin(ra + (p.polar.rot_y * 3.1415)/180) * p.polar.length)
+            self.x = rx + (math.sin(ra + p.polar.rot_x))
+            self.y = ry + (math.cos(ra + p.polar.rot_y))
             self.bearing = p.orientation.rot_y - (90 - p.polar.rot_y - ra)
             self.color = marker.info.marker_type
-
-    def dist(self,x,y,rx,ry):
-        dist = ((rx-x)**2 + (ry-y)**2)**0.5
+    def distance(self,marker):
+        marker.dist
