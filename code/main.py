c18f8fa4565028a4dc22406ded6897e8db197d0e
joe <02joepater06@gmail.com>
2020-03-14 17:21:58 +0000

    startedd mainn loopp

--- code/main.py
+++ code/main.py
@@ -1,8 +1,54 @@
 
 #initialize things
+clawIsUp = False
+eventTimer = -1 #used to time events
 
 while True:
-    
+    if status == "IDLE":
+        status = "CUBES_SEARCHING"
+    else if status == "CUBES_SEARCHING":
+        #search for markers
+        if arena.availableCubes() == 0: #no available cubes
+            status = "CUBES_ROTATING"
+            #---set motors to rotating slowly---
+        else:
+            currentCube = arena.getBestCube()
+            status = "CUBES_ROUTING"
+    else if status == "CUBES_ROTATING":
+        #stop motors
+        time.sleep(0.1)
+        #search for cubes
+        if arena.availableCubes() != 0:
+            currentCube = arena.getBestCube()
+            status = "CUBES_ROUTING"
+            #---stop motors---
+    else if status == "CUBES_ROUTING":
+        route.setRoute(arena.getCurrentPosition(),currentCube.position,corner);
+        if currentCube.isRaised() and not clawIsUp:
+            clawIsUp = True
+            status = "CLAW_LIFTING"
+        else if not currentCube.isRaised() and clawIsUp:
+            clawIsUp = False
+            status = "CLAW_LIFTING"
+        else:
+            status = "GOING_TO_CUBE"
+    else if status == "CLAW_LIFTING":
+        if clawIsUp and currTime-eventTimer>2500: #been >2.5s and claw lifting
+            #stop lift motor
+            status = "GOING_TO_CUBE"
+        elif currTime-eventTimer>2000 and not clawIsUp: #been >2s and claw lowering
+            #stop lift motor
+            status = "GOING_TO_CUBE"
+    elif status == "GOING_TO_CUBE":
+        #search for markers
+        m0,m1 = route.followRoute(arena.currentPos,arena.currOrientation)
+        if m0 == 0 and m1 == 0:
+            status = "CLAW_GRABBING"
+            eventTimer = currTime
+            #start grabbing
+        #set motors
+    elif status == "CLAW_GRABBING":
+        
 
 
 
