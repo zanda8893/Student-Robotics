4cee9a90b2e01054c0509a4e61bd30c9e3a20276
Blitzkiegs03 <f.p.theil@gmail.com>
2020-03-14 17:25:47 +0000

    gettin cube code off luka's branch illegaly

--- claw.py
+++ claw.py
@@ -1,6 +1,5 @@
-import robot
 from sr.robot import *
-import time
+import time as time
 
 class claw(object):
     def __init__(self):
@@ -11,24 +10,30 @@ class claw(object):
         self.grabbed = False
         self.R = Robot()
 
+        self.R.ruggeduino_set_handler_by_fwver("SRcustom",Ruggeduino)
+        self.R.init()
+        self.R.wait_start()
+
+    def buttonPressed(self):
+        self.R.ruggeduinos[0].pin_mode(2, INPUT_PULLUP)
+        if self.R.ruggeduinos[0].digital_read(2) == False:
+            self.R.moters.m0.power = 0
+
     def moveClawUp(self):
         if self.clawUp == False:
-            R.motors[0].m1.power = 100
-            self.clawTimerUp = time.time_ns() + 4*1000*1000
+            self.R.motors[0].m1.power = 100
+            self.clawTimerUp = time()+4
             self.clawUp = True
 
     def moterCheck(self):
-        if time.time_ns() >= self.clawTimerUp():
-            R.motors[0].m1.power = 0
+        if time()>= self.clawTimerUp():
+            self.R.motors[0].m1.power = 0
 
     def moveClawDown(self):
         if self.clawUp == True:
-            R.motors[0].m1.power = 100
-            self.clawTimerUp = time.time_ns() + 4*1000*1000
+            self.R.motors[0].m1.power = 100
+            self.clawTimerUp = time() + 4
             self.clawUp = False
 
-    def grab():
-        if self.grabbing == False
-            R.moter[0].m1.power = 0
-        elif self.grabbed == False:
-            
+    def grab(self):
+        self.R.moters[0].m0.power = 10
---
+++ cube.py
@@ -0,0 +1,12 @@
+import math
+import robot
+
+class Cube():
+    def __init__(self,marker,rx,ry,ra):
+            self.x = rx + (math.cos(ra + (p.polar.rot_y * 3.1415)/180) * p.polar.length)
+            self.y = ry + (math.sin(ra + (p.polar.rot_y * 3.1415)/180) * p.polar.length)
+            self.bearing = p.orientation.rot_y - (90 - p.polar.rot_y - ra)
+            self.color = marker.info.marker_type
+
+    def dist(self,x,y,rx,ry):
+        dist = ((rx-x)**2 + (ry-y)**2)**0.5
