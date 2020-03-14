e27137a03e07edf74f0b6a469aff40fc5f48750f
Shadecorp1 <59394528+Shadecorp1@users.noreply.github.com>
2020-03-14 15:57:48 +0000

    converted degs to rads in cube

--- robot/cube.py
+++ robot/cube.py
@@ -1,8 +1,8 @@
 import math
 class Cube():
     def __init__(self,marker,rx,ry,ra):
-            self.x = rx + (math.sin(ra + p.polar.rot_x))
-            self.y = ry + (math.cos(ra + p.polar.rot_y))
+            self.x = rx + (math.sin(math.radians(ra + p.polar.rot_x)))
+            self.y = ry + (math.cos(math.radians(ra + p.polar.rot_y)))
             self.bearing = p.orientation.rot_y - (90 - p.polar.rot_y - ra)
             self.color = marker.info.marker_type
     def distance(self,marker):
