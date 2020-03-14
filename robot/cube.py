fa1892031a8598e67cd011c9e31384adbbc48414
Shadecorp1 <59394528+Shadecorp1@users.noreply.github.com>
2020-03-14 17:14:37 +0000

    cube now works with Ferdi

--- robot/cube.py
+++ robot/cube.py
@@ -1,10 +1,8 @@
 import math
 class Cube():
     def __init__(self,marker,rx,ry,ra):
-            self.x = rx + (math.sin(math.radians(ra + p.polar.rot_x)))
-            self.y = ry + (math.cos(math.radians(ra + p.polar.rot_y)))
-            self.bearing = p.orientation.rot_y - (90 - p.polar.rot_y - ra)
-            self.color = marker.info.marker_type
-            self.code = marker.info.code
-    def distance(self,marker):
-        self.dist = marker.dist
+        self.x = rx + (marker.dist * math.sin(math.radians(ra + p.polar.rot_y)))
+        self.y = ry + (marker.dist * math.cos(math.radians(ra + p.polar.rot_y)))
+        self.bearing = p.orientation.rot_y - (90 - p.polar.rot_y - ra)
+        self.color = marker.info.marker_type
+        self.id = marker.info.code
