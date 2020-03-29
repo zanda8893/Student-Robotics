26f5d6c9b27162d13c2b32aafcb0e8620e93f32e
Shadecorp1 <lukastarczak@gmail.com>
2020-03-29 15:52:02 +0100

    Fixed Cube as attributes were not correctly called

--- robot/cube.py
+++ robot/cube.py
@@ -1,8 +1,12 @@
 import math
+from sr.robot import *
+p = Point
+
 class Cube():
     def __init__(self,marker,rx,ry,ra):
-        self.x = rx + (marker.dist * math.sin(math.radians(ra + p.polar.rot_y)))
-        self.y = ry + (marker.dist * math.cos(math.radians(ra + p.polar.rot_y)))
-        self.bearing = p.orientation.rot_y - (90 - p.polar.rot_y - ra)
-        self.color = marker.info.marker_type
-        self.id = marker.info.code
+            self.x = rx + (math.sin(ra + p.polar.rot_x))
+            self.y = ry + (math.cos(ra + p.polar.rot_y))
+            self.bearing = p.orientation.rot_y - (90 - p.polar.rot_y - ra)
+            self.color = marker.info.marker_type
+    def distance(self,marker):
+        marker.dist
