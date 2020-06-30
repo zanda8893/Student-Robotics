1b872f534c604165ea6878640dd8bb29938a163b
Shadecorp1 <59394528+Shadecorp1@users.noreply.github.com>
2020-06-30 16:29:49 +0100

    Works out the coordinates of the cube. With an error margin of +- 10cm.

--- code/cube.py
+++ code/cube.py
@@ -1,10 +1,22 @@
 import math
 from sr.robot import *
-p = Point
+
+R = Robot()
+markers = R.see()
 
 class Cube():
-    def __init__(self,marker,rx,ry,ra):
-            self.x = rx + (math.sin(ra + p.polar.rot_x))
-            self.y = ry + (math.cos(ra + p.polar.rot_y))
-            self.bearing = p.orientation.rot_y - (90 - p.polar.rot_y - ra)
-            self.color = marker.info.marker_type
+    def __init__(self,marker,rx,ry):
+            self.x = rx + math.cos((ra) * d)
+            self.y = ry + math.sin((ra) * d)
+            self.color = m.info.marker_type
+
+#robot coords in green zone
+rx = -2.37503
+ry = -2.37503
+
+for m in markers:
+    if m.info.marker_type != MARKER_ARENA:
+        ra = math.radians(m.orientation.rot_y) + math.radians(m.orientation.rot_y)
+        d = m.dist
+        print('Color:',m.info.marker_type,'#:',m.info.code,'dist:',m.dist,'angle:',m.orientation.rot_y)
+        print('x:',Cube(rx,ry,ra).x,'y:',Cube(rx,ry,ra).y)
