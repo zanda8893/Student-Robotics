8dde3d9c06b902da4873d82c5a770fc07f849406
Joe Pater <02joepater06@gmail.com>
2020-07-02 10:22:40 +0100

    Added conversions module to convert to simulator coords

---
+++ code/conversions.py
@@ -0,0 +1,17 @@
+import position
+
+#convert single ordinate to competition value
+def toSimOrd(x):
+    return (x - 5750/2) / 1000
+
+#convert Position from our coordinates to simulator coordinates
+def toSimCoords(p):
+    return Position(toSimOrd(p.x),toSimOrd(p.y))
+
+#convert single ordinate from simulator version to our version
+def fromSimOrd(x):
+    return 5750/2 + (1000 * x)
+
+#convert simulator coordinates to our coordinates
+def fromSimCoords(p):
+    return Position(fromSimOrd(p.x),fromSimOrd(p.y))
