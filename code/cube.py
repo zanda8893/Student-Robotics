c5e427d916f5685fcb58dcee12aa79b8f1ac444d
Joe Pater <02joepater06@gmail.com>
2020-07-02 10:50:31 +0100

    Fixed some maths problems

--- code/cube.py
+++ code/cube.py
@@ -2,17 +2,32 @@ import math
 from sr.robot import *
 from robot_obj import R
 import deg
+from position import Position
 import position
+import conversions
 
 class Cube():
     def __init__(self,marker,rx,ry,ra):
-        self.x = rx + deg.cos(ra - marker.rot_y) * marker.dist*1000
-        self.y = ry + deg.sin(ra - marker.rot_y) * marker.dist*1000
-        self.a = (marker.orientation.rot_y + ra) % 360
+        #ra is the robot's angle, measured anticlockwise from x-axis
+        
+        #ra - marker.rot_y is the angle between the x-axis and the
+        #line between the cube and the robot
+
+        #ra - marker.orientation.rot_y is the anticlockwise angle
+        #between the negative x-axis and the normal to the marker
+
+        #self.a is the anticlockwise angle between the marker's normal
+        #and the positive x-axis
+
+        self.a = (ra - marker.orientation.rot_y + 180) % 360
+        self.x = rx + deg.sin(ra - marker.rot_y) * marker.dist*1000
+        self.y = ry + deg.cos(ra - marker.rot_y) * marker.dist*1000
         self.color = m.info.marker_type
+
     #functions that define how the cube is printed
     def __str__(self):
         return "x: {0}  y: {1}  col: {2}".format(self.x,self.y,self.color)
+
     def __repr__(self):
         return self.__str__()
     
@@ -25,4 +40,5 @@ while True:
     for m in markers:
         #make cube, print cube
         c = Cube(m,robot_x,robot_y,robot_a)
-        print("Code: {0}  {1}".format(m.info.code,c))
+        p = conversions.toSimCoords(Position(c.x,c.y))
+        print("Code: {0}  {1}".format(m.info.code,p))
