718cee1cdd5bdf14685f66982a0352a23b402e28
Joe Pater <02joepater06@gmail.com>
2020-07-03 17:31:22 +0100

    Added template for dropping position (dropping.py)

---
+++ code/dropping.py
@@ -0,0 +1,13 @@
+
+
+spaces = [] #TODO: fill in with spaces
+num_used_spaces = 0
+
+def locationForCubeZone0():
+    pass
+
+def transformLocationToZone(zone):
+    pass
+
+def locationForCube():
+    pass
