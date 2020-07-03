eb2f08aef8b10127e044b715b36d09eb3b54dada
Joe Pater <02joepater06@gmail.com>
2020-07-03 17:31:22 +0100

    Added template for dropping position (dropping.py)

---
+++ code/dropping.py
@@ -0,0 +1,18 @@
+
+
+spaces = [] #TODO: fill in with spaces
+#number of spaces in previous list already used up
+num_used_spaces = 0
+
+#Find a suitable location to place the next cube for zone 0
+def locationForCubeZone0():
+    pass
+
+#Translate the position depending on the zone
+#e.g. (300,200) -> (300,5750-200) for zone 1
+def transformLocationToZone(zone):
+    pass
+
+#Gets location for cube depending on zone
+def locationForCube():
+    pass
