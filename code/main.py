774edd486691f80ef46ac2bdf982e27c92b9e4db
zanda8893 <zanda.hallard@outlook.com>
2020-07-10 19:38:18 +0100

    More changes to mail loop

--- code/main.py
+++ code/main.py
@@ -3,10 +3,11 @@
 #~~ = Yet to be implimented/written
 
 #Imports
-import lift, claw, position, route
+import lift, claw, position, route_new
 from robot_obj import R
 from arena import A
 from sr.robot import *
+
 
 
 while True:
@@ -18,26 +19,30 @@ while True:
 
     #Look for markers
     markers = R.see()
-
+    print("See")
     #Find robot position
     #Rp = Robot coordinates, Ra = Robot angle
+    print("pos")
     Rp,Ra = position.findPosition(markers)
-    A.addMarkers(markers,Rp,Ra)
+    #print("arena")
+    #A.addMarkers(markers,Rp,Ra)
 
     #Find nearest cube
     #colour either MARKER_TOKEN_GOLD or MARKER_TOKEN_SILVER
-    cube = A.getNearest(Rp,MARKER_TOKEN_GOLD)
+    #print("get near")
+    #cube = A.getNearest(Rp,MARKER_TOKEN_GOLD)
 
-    print(cube)
-
+    #print(cube)
+    """
     if cube.p.onPlatform():
         lift.raiseLift()
     else:
         lift.lowerLift()
-
+    """
+    #print(cube.code)
     #Go to nearest cube
-    #res = orienting.goToCube(cube.code)
-    route.goToPointSync(cube.code)
+    #~~res = orienting.goToCube(cube.code)
+    route_new.goToCube("test")
 
     """
     if not res:
