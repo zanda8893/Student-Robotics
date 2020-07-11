a02c572600107108bb2602863b8b128dc677f828
zanda8893 <zanda.hallard@outlook.com>
2020-07-11 02:56:11 +0100

    Now uses coordinates and added zone converstion (Needs test)

--- code/route_new.py
+++ code/route_new.py
@@ -1,34 +1,35 @@
-import drive, robot_obj, conversions, orienting
+import drive, robot_obj, conversions, orienting, position, claw, route
 
+xh = 100
+yh = 750
 
-def convert(zone):
-    if zone == 0:
-         p1 = conversions.fromSimCoords() #Position 1
-         pC1 = conversions.fromSimCoords(Position(-p.x,-p.y)) #Position cube 1
-    if zone == 1:
-         p1 = conversions.fromSimCoords(Position(p.x,-p.y))
-         pC1 = conversions.fromSimCoords(Position(-p.x,-p.y))
-    if zone == 2:
-         p1 = conversions.fromSimCoords(Position(p.x,p.y))
-         pC1 = conversions.fromSimCoords(Position(-p.x,-p.y))
-    if zone == 3:
-         p1 = conversions.fromSimCoords(Position(-p.x,p.y))
-         pC1 = conversions.fromSimCoords(Position(-p.x,-p.y))
+x = 1900
+y = 1400
+a = 90
+
+x1 = 2350
+y1 = 4425
+
+if robot_obj.R.zone != 0:
+     position.translateToZone(position.Position(x,y),robot_obj.R.zone)
 
 
 def getCube(c):
     og_Rp = position.findPosition(robot_obj.R.see())
-    if c == (code=32,color=MARKER_TOKEN_GOLD,p=Position(3775,3775),a=0) or p == (code=33,color=MARKER_TOKEN_GOLD,p=Position(3775,1975),a=0) or p == (code=34,color=MARKER_TOKEN_GOLD,p=Position(1975,1975),a=0) or p == (code=35,color=MARKER_TOKEN_GOLD,p=Position(1975,3775),a=0):
+    if c.code >= 32 and c.code <= 35:
         print("Rotate")
-        drive.driveRotateToAngle(0)
+        route.goToPointStraight(og_Rp[0],position.Position(x,y))
+        drive.driveRotateAngle(a)
+        drive.driveRotateToAngle(a)
         robot_obj.R.sleep(0.9)
-        drive.driveStraightSync(50,2.35)
-        drive.driveRotateAngle(90)
-        drive.driveRotateToAngle(89)
-        robot_obj.R.sleep(0.9)
-        drive.driveStraightSync(60,1.7)
         orienting.approachCube()
-        if robot_obj.R.ruggeduinos[0].digital_read(4) == False:
+        claw.grabClawSync()
+        Rp = position.findPosition(robot_obj.R.see())
+        route.goToPointStraight(Rp[0],position.Position(xh,yh))
+        if robot_obj.R.ruggeduinos[0].digital_read(5) == False:
             drive.driveStraightSync(-30,2)
-            route.goToPointStraight(og_Rp)
-            goToCube(p)
+            Rp = position.findPosition(robot_obj.R.see())
+            route.goToPointStraight(Rp[0],og_Rp)
+            getCube(c)
+    else:
+        route.goToPointStraight(og_Rp[0],position.Position(x1,y1))
