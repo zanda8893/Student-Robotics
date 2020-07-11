569ff8e3046f22a0e4ec679280ec1906822d5490
zanda8893 <zanda.hallard@outlook.com>
2020-07-11 06:34:45 +0100

    Code for all 4 lower gold cubes

--- code/main.py
+++ code/main.py
@@ -3,39 +3,54 @@
 #~~ = Yet to be implimented/written
 
 #Imports
-import lift, claw, position, route_new, route, robot_obj
+import lift, claw, position, route_new, route, robot_obj,dropping, drive
 from arena import A
 from sr.robot import *
 
+count = 0
 
-#Start
+while True:
+    #Start
+    #Look for markers
+    count = count + 1
+    if count == 1:
+        robot_obj.R.sleep(2)
+        markers = robot_obj.R.see()
+        print("See")
+        #Find robot position
+        #Rp = Robot coordinates, Ra = Robot angle
+        print("pos",markers)
+        start_Rp,start_Ra = position.findPosition(markers)
+        #Find nearest cube
+        #colour either MARKER_TOKEN_GOLD or MARKER_TOKEN_SILVER
+        #print("get near")
+        print("test2")
+        cube = A.getNearest(start_Rp,MARKER_TOKEN_GOLD)
+        print(cube,"test")
+        print("test1")
+        route_new.getCube(cube)
+        robot_obj.R.sleep(1)
+        claw.openClaw()
+        drive.driveStraightSync(-40,3)
+        drive.driveRotateToAngle(0)
+        A.removeCube(cube)
 
-#Set claw and lift to known positions
-
-
-#Look for markers
-markers = robot_obj.R.see()
-print("See")
-#Find robot position
-#Rp = Robot coordinates, Ra = Robot angle
-print("pos")
-start_Rp,start_Ra = position.findPosition(markers)
-#print("arena")
-#A.addMarkers(markers,start_Rp,start_Ra)
-
-#Find nearest cube
-#colour either MARKER_TOKEN_GOLD or MARKER_TOKEN_SILVER
-#print("get near")
-cube = A.getNearest(start_Rp,MARKER_TOKEN_GOLD)
-
-route_new.getCube(cube)
-claw.grabClaw()
-robot_obj.R.sleep(1)
-Rp = position.findPosition(robot_obj.R.see())
-if Rp is None:
-    print("Pos Failed")
-else:
-    route.goToPointStraight(Rp[0],start_Rp)
-
-print("Done")
-#End
+        print("Done")
+        #End
+    elif count== 2:
+        route_new.getCube2()
+        claw.openClaw()
+        drive.driveStraightSync(-40,3)
+        drive.driveRotateToAngle(0)
+    elif count == 3:
+        route_new.getCube3()
+        claw.openClaw()
+        drive.driveStraightSync(-40,3)
+        drive.driveRotateAngle(180)
+        drive.driveRotateToAngle(0)
+    else:
+        route_new.getCube4()
+        claw.openClaw()
+        drive.driveStraightSync(-40,3)
+        drive.driveRotateAngle(180)
+        drive.driveRotateToAngle(0)
--- code/route_new.py
+++ code/route_new.py
@@ -1,17 +1,38 @@
-import drive, robot_obj, conversions, orienting, position, claw, route
+import drive, robot_obj, conversions, orienting, position, claw, route, lift
 
 xh = 100
 yh = 750
+ah = 225
 
-x = 1900
-y = 1400
+x = 1850
+y = 700
 a = 90
 
-x1 = 2350
-y1 = 4425
+x1 = 2000
+y1 = 3300
+ah2 = 245
+
+x2 = 3300
+y2 = 2000
+ah3 = 210
+
+x3 = 700
+y3 = 1850
+
+x4 = 3400
+
 
 if robot_obj.R.zone != 0:
-     position.translateToZone(position.Position(x,y),robot_obj.R.zone)
+     x,y = position.translateToZone(position.Position(x,y),robot_obj.R.zone)
+     a = bearingToZone(a)
+     xh,yh = position.translateToZone(position.Position(xh,yh),robot_obj.R.zone)
+     ah = bearingToZone(ah)
+     x1, y1 = position.translateToZone(position.Position(x1,y1),robot_obj.R.zone)
+     ah2 = bearingToZone(ah2)
+     x2, y2 = position.translateToZone(position.Position(x2,y2),robot_obj.R.zone)
+     ah3 = bearingToZone(ah3)
+     x3, y3 = position.translateToZone(position.Position(x3,y3),robot_obj.R.zone)
+     x4, y4 = position.translateToZone(position.Position(x4,y4),robot_obj.R.zone)
 
 
 def getCube(c):
@@ -22,14 +43,53 @@ def getCube(c):
         drive.driveRotateAngle(a)
         drive.driveRotateToAngle(a)
         robot_obj.R.sleep(0.9)
+        route.goToPointStraight(og_Rp[0],position.Position(x,y+650))
         orienting.approachCube()
         claw.grabClawSync()
-        Rp = position.findPosition(robot_obj.R.see())
-        route.goToPointStraight(Rp[0],position.Position(xh,yh))
+        drive.driveRotateToAngle(ah)
+        drive.driveStraightSync(40,10)
         if robot_obj.R.ruggeduinos[0].digital_read(5) == False:
+            print("Get cube failed")
             drive.driveStraightSync(-30,2)
             Rp = position.findPosition(robot_obj.R.see())
             route.goToPointStraight(Rp[0],og_Rp)
             getCube(c)
-    else:
-        route.goToPointStraight(og_Rp[0],position.Position(x1,y1))
+
+def getCube2():
+    robot_obj.R.sleep(1)
+    og_Rp = position.findPosition(robot_obj.R.see())
+    route.goToPointStraight(og_Rp[0],position.Position(x,y))
+    og_Rp = position.findPosition(robot_obj.R.see())
+    route.goToPointStraight(og_Rp[0],position.Position(x1,y1))
+    drive.driveRotateToAngle(90)
+    orienting.approachCube()
+    claw.grabClawSync()
+    drive.driveRotateToAngle(ah2)
+    drive.driveStraightSync(40,10)
+
+def getCube3():
+    robot_obj.R.sleep(1)
+    og_Rp = position.findPosition(robot_obj.R.see())
+    route.goToPointStraight(og_Rp[0],position.Position(x3,y3))
+    og_Rp = position.findPosition(robot_obj.R.see())
+    route.goToPointStraight(og_Rp[0],position.Position(x2,y2))
+    orienting.approachCube()
+    claw.grabClawSync()
+    drive.driveRotateToAngle(ah3)
+    drive.driveStraightSync(30,13)
+
+def getCube4():
+    robot_obj.R.sleep(1)
+    og_Rp = position.findPosition(robot_obj.R.see())
+    route.goToPointStraight(og_Rp[0],position.Position(x3,y3))
+    og_Rp = position.findPosition(robot_obj.R.see())
+    route.goToPointStraight(og_Rp[0],position.Position(x2,y2))
+    drive.driveStraightSync(60,2)
+    og_Rp = position.findPosition(robot_obj.R.see())
+    route.goToPointStraight(og_Rp[0],position.Position(x4,y1))
+    orienting.approachCube()
+    claw.grabClawSync()
+    og_Rp = position.findPosition(robot_obj.R.see())
+    route.goToPointStraight(og_Rp[0],position.Position(x2,y2))
+    drive.driveRotateToAngle(ah3)
+    drive.driveStraightSync(40,10)
